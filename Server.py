4a1e4c31d8a0ee20b1a9aa6711f7827398a1a214
Lexxsage <55046837+Lexxsage@users.noreply.github.com>
2020-09-23 16:03:40 +0700

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,9 @@
+import zmq
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://127.0.0.1:4050")
+
+while True:
+    msg = socket.recv()
+    print ("Got", msg.decode())
+    socket.send(msg)
