17d716494991099ae54b61e7c44bb857f8522472
Lexxsage <55046837+Lexxsage@users.noreply.github.com>
2020-09-30 10:10:42 +0700

    Update 17.sh

--- 17.sh
+++ 17.sh
@@ -1,4 +1,12 @@
+in terminal do
 sudo iptables -F
 sudo iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -i eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A INPUT -i eth0 -j DROP
+
+when try to 
+
+ssh -L 8070:www.google.com:22 <hostname>
+w - look your pts/number
+to kill:
+pkill -9 -t pts/number
